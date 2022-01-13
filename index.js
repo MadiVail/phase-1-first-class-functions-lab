fa850acb271c133210743cd421b5e7d18f0794a6
Madisyn Vail <virtualmadi3@gmail.com>
2022-01-13 09:15:14 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(drivers.length - 2, drivers.length);
+}
+
+var selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+function createFareMultiplier(multi) {
+    return function fareMultiplier(fare){ 
+        return multi * fare;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+
